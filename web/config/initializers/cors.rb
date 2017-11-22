7121939229c7ceb4846f3c0fa8e0a3ad2e81e2e7
Jade Dominguez <plusjade@gmail.com>
2017-11-22 14:28:38 -0800

    add www.positivebuddy.com to cors

--- web/config/initializers/cors.rb
+++ web/config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "localhost:3000", "www.makeist.com", "localhost:5000"
+    origins "localhost:3000", "www.makeist.com", "localhost:5000", "www.positivebuddy.com"
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
