cf9e6c93bf3b7da8a4bf72dc241f461cffb22637
Jade Dominguez <plusjade@gmail.com>
2017-12-30 18:34:40 -0800

    Update sample bot messaging.

--- web/app/controllers/chats_controller.rb
+++ web/app/controllers/chats_controller.rb
@@ -1,16 +1,18 @@
 class ChatsController < ActionController::Base
   def index
     render json: {
-      chatsIndex: [1],
+      chatsIndex: [],
       chatsObjects: home.reduce({}) { |memo, a| memo[a[:id]] = a ; memo },
       chatsCommands: [
-        # {id: 1, duration: 500, delay: 300},
-        {id: 2, duration: 800, delay: 1000},
+        {id: 1, duration: 1000, delay: 300},
+        {id: 2, duration: 800, delay: 2000},
         {id: 3, duration: 800, delay: 1000},
-        {id: 4},
-        {id: 5},
-        {id: 6},
-        {id: 7},
+        {id: 4, duration: 800, delay: 800},
+        {id: 5, duration: 800, delay: 800},
+        {id: 6, duration: 800, delay: 1000},
+        {id: 7, duration: 800, delay: 1000},
+        {id: 8, duration: 1500, delay: 1000},
+        {id: 9, duration: 1500, delay: 2000},
       ]
     }
   end
@@ -28,43 +30,58 @@ class ChatsController < ActionController::Base
       {
         id: 2,
         type: "botEntry",
-        value: "Every day I'm going to ask you 'What did you do today?' " \
-               "I'll keep track of all your answers and help you build positive habits.",
+        value: "I'm positive buddy 🤪" \
+               "",
         timestamp: Time.now + 10.seconds.to_i,
         type: "theirs",
       },
       {
         id: 3,
         type: "botEntry",
-        value: "This is a new message YAY \o/",
+        value: "💡💡💡 I'm not that bright....",
         emoji: "",
         type: "theirs",
       },
       {
         id: 4,
         type: "botEntry",
-        value: "Sup sup sup",
+        value: "🤔",
         emoji: "",
         type: "theirs",
       },
+
       {
         id: 5,
         type: "botEntry",
-        value: "...",
+        value: "yet!",
         emoji: "",
         type: "theirs",
       },
       {
         id: 6,
         type: "botEntry",
-        value: "meep meep meep!",
+        value: "But over time I'll learn more about the world.",
         emoji: "",
         type: "theirs",
       },
       {
         id: 7,
         type: "botEntry",
-        value: "-_________-",
+        value: "And help you on your journey.",
+        emoji: "",
+        type: "theirs",
+      },
+      {
+        id: 8,
+        type: "botEntry",
+        value: "Let's do it! 💪",
+        emoji: "",
+        type: "theirs",
+      },
+      {
+        id: 9,
+        type: "botEntry",
+        value: "btw, I'm a feminist ^_^",
         emoji: "",
         type: "theirs",
       },
