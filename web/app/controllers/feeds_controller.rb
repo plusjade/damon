b4d92266d05c977f64a187638d927e031e48c5d5
Jade Dominguez <plusjade@gmail.com>
2018-01-02 19:49:31 -0800

    Test data for bot message inside the Feed.

--- web/app/controllers/feeds_controller.rb
+++ web/app/controllers/feeds_controller.rb
@@ -4,6 +4,102 @@ class FeedsController < ActionController::Base
     feed = Feed.new(user_id: user.id, category_name: params[:category_name])
     feed = feed.feed
 
+    feed.insert(2,
+      {
+        id: 1,
+        type: "botEntry",
+        value: "Hello there! 👋",
+        emoji: "🤖",
+        timestamp: Time.now.to_i,
+      })
+
+    feed.insert(5,
+      {
+        id: 1,
+        type: "botEntry",
+        value: "Hello there! 👋",
+        emoji: "🤖",
+        timestamp: Time.now.to_i,
+      })
+
+    feed.insert(10,
+      {
+        id: 1,
+        type: "botEntry",
+        value: "Hello there! 👋",
+        emoji: "🤖",
+        timestamp: Time.now.to_i,
+      })
+
+    feed.insert(15,
+      {
+        id: 1,
+        type: "botEntry",
+        value: "Hello there! 👋",
+        emoji: "🤖",
+        timestamp: Time.now.to_i,
+      })
+
+    feed =     [
+      {
+        id: 1,
+        type: "botEntry",
+        value: "Hello there! 👋",
+        emoji: "🤖",
+        timestamp: Time.now.to_i,
+      },
+      {
+        id: 2,
+        type: "botEntry",
+        value: "I'm positive buddy 🤪",
+        timestamp: (Time.now + 10.seconds).to_i,
+      },
+      {
+        id: 3,
+        type: "botEntry",
+        value: "💡💡💡 I'm not that bright....",
+        emoji: "",
+      },
+      {
+        id: 4,
+        type: "botEntry",
+        value: "🤔",
+        emoji: "",
+      },
+
+      {
+        id: 5,
+        type: "botEntry",
+        value: "yet!",
+        emoji: "",
+      },
+      {
+        id: 6,
+        type: "botEntry",
+        value: "But over time I'll learn more about the world.",
+        emoji: "",
+
+      },
+      {
+        id: 7,
+        type: "botEntry",
+        value: "And help you on your journey.",
+        emoji: "",
+      },
+      {
+        id: 8,
+        type: "botEntry",
+        value: "Let's do it! 💪",
+        emoji: "",
+      },
+      {
+        id: 9,
+        type: "botEntry",
+        value: "btw, I'm a feminist ^_^",
+        emoji: "",
+      },
+    ] + feed
+
     render json: {
       feed: feed,
     }
