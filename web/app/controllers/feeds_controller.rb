5d5a0de7135bd1dcc22127b7b62fe293254ee076
Jade Dominguez <plusjade@gmail.com>
2018-01-06 00:50:37 -0800

    Try placing summaries as bot messages.

--- web/app/controllers/feeds_controller.rb
+++ web/app/controllers/feeds_controller.rb
@@ -3,13 +3,24 @@ class FeedsController < ActionController::Base
     user = User.find(params[:user_id])
     f = Feed.new(user_id: user.id, category_name: params[:category_name])
     feed = f.feed
-    chats = get_chats
+    chats = begin
+      CategoryList.new(user_id: user.id).data_for_category(params[:category_name])[:summaries].map.with_index do |value, i|
+        {
+          id: Digest::MD5.hexdigest(value),
+          type: "botEntry",
+          value: value,
+          emoji: i.zero? ? "🤖" : nil,
+        }
+      end
+    end
+
     render json: {
-      chatsIndex: feed.map{ |a| a[:id] } ,
+      chatsIndex: (feed + chats).map{ |a| a[:id] } ,
       chatsObjects: (feed + chats).reduce({}) { |memo, a| memo[a[:id]] = a ; memo },
       chatsCommands: [
-        # {id: chats.first[:id], duration: 2000, delay: 300},
-        # {id: chats[1][:id], duration: 1600, delay: 1000},
+        # {id: chats[0][:id], duration: 300, delay: 300},
+        # {id: chats[1][:id], duration: 300, delay: 300},
+        # {id: chats[2][:id], duration: 300, delay: 300},
         # {id: 3, duration: 1000, delay: 1000},
         # {id: 4, duration: 800, delay: 1000},
         # {id: 5, duration: 1000, delay: 1000},
--- web/app/models/category_list.rb
+++ web/app/models/category_list.rb
@@ -3,6 +3,17 @@ class CategoryList
 
   attr_reader :user_id
 
+  EMOJIS = {
+    1 => "🔥🔥🚀",
+    2 => "🔥🔥",
+    3 => "🔥",
+    4 => "🌱🌱",
+    5 => "🌱",
+    6 => "🤒",
+    7 => "😴",
+    1_000 => "👶",
+  }
+
   def initialize(user_id:)
     @user_id = user_id
   end
@@ -22,32 +33,25 @@ class CategoryList
   def days_since_last(category)
     category_id = category_ids_by_name[category]
     entry = Entry.descending.where(user_id: user_id, category_id: category_id).first
-    entry ? entry.days_ago : 0
+    entry ? entry.days_ago : nil
   end
 
   def days_since_first(category)
     category_id = category_ids_by_name[category]
     entry = Entry.ascending.where(user_id: user_id, category_id: category_id).first
-    entry ? entry.days_ago : 0
+    entry ? entry.days_ago : nil
   end
-
-  EMOJIS = {
-    1 => "🔥🔥🚀",
-    2 => "🔥🔥",
-    3 => "🔥",
-    4 => "🌱🌱",
-    5 => "🌱",
-    6 => "🤒",
-    7 => "😴",
-  }
 
   def data_for_category(category)
     total_entries = category_totals_by_name[category]
     total_entries_last_7 = category_totals_by_name_last_7[category]
     days_since_last = days_since_last(category)
-    emoji = nil
+    emoji = 1_000
 
-    if days_since_last <= 3
+
+    if days_since_last.nil?
+      # noop since category is new
+    elsif days_since_last <= 3
       if total_entries >= 10
         emoji = 1
       elsif total_entries >= 5
@@ -69,25 +73,37 @@ class CategoryList
       emoji = 7
     end
 
-    active = if days_since_last.zero?
+    active = if days_since_last.nil?
+                nil
+              elsif days_since_last.zero?
                 "today"
               else
                 "#{time_ago_in_words(days_since_last.days.ago)} ago"
               end
 
-    first_entry_in_words = time_ago_in_words(days_since_first(category).days.ago)
+    summary = "new"
+    if active
+      summary = "active #{active}"
+    end
+
+    first_entry_in_words = nil
+    days_since_first = days_since_first(category)
+    if days_since_first
+      first_entry_in_words = time_ago_in_words(days_since_first.days.ago)
+    end
 
     {
       name: category,
-      summary: "active #{active}",
+      summary: summary,
       total: total_entries,
       emoji_id: emoji,
       emoji: EMOJIS[emoji],
       daysAgo: days_since_last,
       summaries: [
-        "Your last entry was #{active}",
-        ("You've created #{total_entries_last_7} entries in the last 7 days" unless total_entries_last_7.zero?),
-        "You've created #{total_entries} total entries over the past #{first_entry_in_words}"
+        ("Your adventure is brand new! #{EMOJIS[emoji]}" if days_since_last.nil?),
+        ("Your last entry was #{active} #{EMOJIS[emoji]}" if active),
+        ("You've added #{total_entries_last_7} entries in the last 7 days" unless total_entries_last_7.zero?),
+        ("You've added #{total_entries} total entries over the past #{first_entry_in_words}" unless total_entries.zero?),
       ].compact
     }
   end
