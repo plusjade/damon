8f4f4d5c181115a8cdf83379dc00ecde85624ed5
Jade Dominguez <plusjade@gmail.com>
2018-01-12 23:00:55 -0800

    Change date format to FRI 1/12

--- web/app/models/ordinal.rb
+++ web/app/models/ordinal.rb
@@ -1,7 +1,7 @@
 class Ordinal
   PT = ActiveSupport::TimeZone["Pacific Time (US & Canada)"]
   ORDINAL_FORMAT = "%Y-%j".freeze
-  DATE_FORMAT = "%a %^b %d".freeze
+  DATE_FORMAT = "%a %_m/%d".freeze
 
   def self.from_time(time)
     time.in_time_zone(PT).strftime(ORDINAL_FORMAT)
