ec29e632c6ff04c35fce13f5e8300d17753789e4
Jade Dominguez <plusjade@gmail.com>
2018-01-24 01:07:26 -0800

    Hack out quick way to update/edit categories after entry save.

--- web/app/controllers/entries_controller.rb
+++ web/app/controllers/entries_controller.rb
@@ -29,6 +29,25 @@ class EntriesController < ApplicationController
     }
   end
 
+  def update
+    entry = Entry.where(user_id: current_user.id).find(params[:id])
+
+    category = nil
+    category_name = params[:category].to_s.strip.downcase.presence
+    category_name = nil if category_name == "all"
+    category_name ||= "uncategorized"
+    category = Category.find_or_create_by!(name: category_name, user: current_user)
+    raise ActiveRecord::RecordNotFound unless category
+
+    entry.category = category
+    entry.save!
+
+    render json: {
+      entry: EntrySerializer.new(entry),
+      category: entry.category.name
+    }
+  end
+
   def destroy
     entry = Entry.where(user_id: current_user.id).find(params[:id])
     entry.destroy
